ac7566797686e977530846305fe9d2d4c116ed02
Victor Wanderley <victorwanderley1>
2022-08-27 16:43:32 -0300

    draft: Primeiro Commit - Arquivos de Configuracao e Afins

---
+++ src/main/java/br/com/wanderley/victor/pem/PersonalExpenseManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.wanderley.victor.pem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalExpenseManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalExpenseManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wanderley/victor/pem/PersonalExpenseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wanderley.victor.pem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalExpenseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
